a2582ff585e8b5812142f0efb820331b96afd21a
Tautvydas Rasimavicius <tautvydas.rasimavicius@gmail.com>
2017-07-12 23:50:34 +0300

    Git setup

---
+++ git/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install or update git
+sudo apt-get install -y git
+
+# Global settings
+git config --global user.name "Tautvydas Rasimavicius"
+git config --global user.email "tautvydas.rasimavicius@gmail.com"
+
+git config --global core.editor vim
+git config --global core.excludesfile ~/.dotfiles/git/.gitignore
+git config --global push.default tracking
+git config --global pull.rebase true
+git config --global color.ui true
