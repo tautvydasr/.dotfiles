dfe27704e8de70e17868415cf2c65bc163ae9e5f
Tautvydas Rasimavicius <tautvydas.rasimavicius@gmail.com>
2019-05-06 18:08:53 +0300

    Add default git gpg program

--- git/setup.sh
+++ git/setup.sh
@@ -12,6 +12,7 @@ git config --global core.excludesfile ~/.dotfiles/git/.gitignore
 git config --global push.default tracking
 git config --global pull.rebase true
 git config --global color.ui true
+git config --global gpg.program gpg
 git config --global commit.gpgsign true
 git config --global pager.branch false
 
