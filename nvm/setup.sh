68bed83bec0515f495498280262dccd09e8cc389
Tautvydas Rasimavicius <tautvydas.rasimavicius@gmail.com>
2019-05-20 21:19:09 +0300

    Setup node version manager (nvm)

---
+++ nvm/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd $HOME
+git clone https://github.com/nvm-sh/nvm.git .nvm
+cd "$HOME/.nvm"
+git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+sh ./nvm.sh
+
