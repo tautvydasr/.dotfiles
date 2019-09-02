35c648e5a07ecfb02463c55c4cbd12defc0997da
Tautvydas Rasimavicius <tautvydasr@opentable.com>
2019-09-02 10:21:13 +0300

    Install missing avn libs

--- nvm/setup.sh
+++ nvm/setup.sh
@@ -6,6 +6,6 @@ cd "$HOME/.nvm"
 git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 sh ./nvm.sh
 
-npm install -g avn avn-nvm
+npm install -g avn avn-nvm avn-n
 avn setup
 
