3f0b24be000cd06ffea2df927bbcb938aea13fab
Tautvydas Rasimavicius <tautvydas.rasimavicius@gmail.com>
2022-10-25 18:06:34 +0300

    Use buit-in vscode settings sync

--- vscode/setup.sh
+++ vscode/setup.sh
@@ -3,30 +3,9 @@
 source ~/.dotfiles/scripts/os.sh
 
 if is_linux; then 
-    VSCODE_BIN_DIR=""
-    VSCODE_SETTINGS_DIR=""
-    echo "Not implemented yet...";
+    sudo snap install --classic code
 fi    
 
 if is_mac; then 
-    VSCODE_BIN_DIR="$HOME/Applications/Visual Studio Code.app/Contents/Resources/app/bin/"
-    VSCODE_SETTINGS_DIR="$HOME/Library/Application Support/Code/User"
     brew install visual-studio-code
 fi    
-
-# Sync vscode settings
-rm -f "$VSCODE_SETTINGS_DIR/settings.json" &> /dev/null
-rm -f "$VSCODE_SETTINGS_DIR/keybindings.json" &> /dev/null
-ln -s ~/.dotfiles/vscode/settings.json "$VSCODE_SETTINGS_DIR/settings.json"
-ln -s ~/.dotfiles/vscode/keybindings.json "$VSCODE_SETTINGS_DIR/keybindings.json"
-
-# Install vscode extensions
-VSCODE_EXTENSIONS_FILE=~/.dotfiles/vscode/extensions.list
-if [ -d "$VSCODE_BIN_DIR" ] && [[ -x "$VSCODE_BIN_DIR/code" ]] && [[ -f "$VSCODE_EXTENSIONS_FILE" ]]; then
-    cd "$VSCODE_BIN_DIR"
-    for ext in $(cat $VSCODE_EXTENSIONS_FILE); do
-        code --install-extension "$ext"
-    done    
-    cd - &> /dev/null
-fi
-
