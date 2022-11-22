1fb8767b8273e9a5b8d0060031d66d4b6269180c
Tautvydas Rasimavicius <tautvydas.rasimavicius@gmail.com>
2022-11-22 19:21:30 +0200

    Setup packer

--- nvim/config/lua/plugins.lua
+++ nvim/config/lua/plugins.lua
@@ -1,1 +1,30 @@
-print("Setup plugins here...")
+-- Install packer itself
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = vim.fn.system({
+      'git',
+      'clone',
+      '--depth',
+      '1',
+      'https://github.com/wbthomason/packer.nvim',
+      install_path
+  })
+  print('Installing packer on "'..install_path..'" close and reopen Neovim')
+end
+
+local status_ok, packer = pcall(require, 'packer')
+
+if not status_ok then
+  return
+end
+
+return packer.startup(function(use)
+  -- Plugins
+  use 'wbthomason/packer.nvim'
+
+  -- Sync packer automatically
+  if PACKER_BOOTSTRAP then
+    require('packer').sync()
+  end
+end)
