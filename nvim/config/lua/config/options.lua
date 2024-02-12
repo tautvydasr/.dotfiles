9100891e7cf8da0e928ec225b9adab1de3848999
Tautvydas Rasimavicius <tautvydas.rasimavicius@gmail.com>
2024-02-12 19:36:43 +0200

    Rm comment

--- nvim/config/lua/config/options.lua
+++ nvim/config/lua/config/options.lua
@@ -1,6 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
 
 vim.opt.scrolloff = 15 -- Lines of context
 vim.opt.colorcolumn = "120" -- Mark virtual page size
