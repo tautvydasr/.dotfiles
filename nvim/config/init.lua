d00266221a25f1a42df17f080029c53cb90ea72a
Tautvydas <15105206+tautvydasr@users.noreply.github.com>
2024-02-12 19:37:29 +0200

    Setup LazyVim (#3)
    * Setup LazyVim
    
    * Sync plugins
    
    * Rm comment

--- nvim/config/init.lua
+++ nvim/config/init.lua
@@ -1,3 +1,2 @@
-require("settings")
-require("plugins")
-require("commands")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ nvim/config/lua/config/options.lua
@@ -0,0 +1,10 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
+vim.opt.scrolloff = 15 -- Lines of context
+vim.opt.colorcolumn = "120" -- Mark virtual page size
+
+vim.opt.spelllang = "en_us"
+vim.opt.spell = true
+
+vim.opt.clipboard = "" -- Set to default registries
