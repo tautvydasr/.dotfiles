f324abb7defe7e54fe94c46453c3296cb4288703
Tautvydas Rasimavicius <tautvydas.rasimavicius@gmail.com>
2024-02-19 18:39:39 +0200

    Use trouble for location and quickfix lists

---
+++ nvim/config/lua/plugins/trouble.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/trouble.nvim",
+  keys = {
+    { "<leader>xL", false },
+    { "<leader>xl", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
+    { "<leader>xQ", false },
+    { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
+  },
+}
