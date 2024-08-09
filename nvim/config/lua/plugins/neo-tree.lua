8d1a58bad787cbfffec2189f677377906e4b1837
Tautvydas Rasimavicius <tautvydas.rasimavicius@gmail.com>
2024-08-09 19:47:25 +0300

    Do not toggle Neotree

--- nvim/config/lua/plugins/neo-tree.lua
+++ nvim/config/lua/plugins/neo-tree.lua
@@ -1,5 +1,14 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = false, reveal = true })
+      end,
+      desc = "Explorer NeoTree (Root Dir)",
+    },
+  },
   opts = {
     filesystem = {
       filtered_items = {
