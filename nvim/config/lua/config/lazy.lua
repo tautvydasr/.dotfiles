a7a3858fd1479594095d0316ebf7bf87cec203ec
Tautvydas Rasimavicius <tautvydasr@opentable.com>
2024-08-14 08:28:10 +0300

    Enable Java language features

--- nvim/config/lua/config/lazy.lua
+++ nvim/config/lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.lang.java" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
