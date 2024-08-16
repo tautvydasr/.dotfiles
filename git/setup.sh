c456b2e5beb1c72fcb87146beba0a2b0beef1fc7
Tautvydas Rasimavičius <tautvydasr@opentable.com>
2024-08-16 15:26:43 +0300

    Git partial-push alias (optional)

--- git/setup.sh
+++ git/setup.sh
@@ -17,3 +17,8 @@ git config --global commit.gpgsign true
 git config --global pager.branch false
 git config --global pager.config false
 git config --global init.defaultBranch main
+
+# Add the following to .gitconfig file to enable partial-push
+# [alias]
+#     # Push large repositories in smaller chunks. Usage: git partial-push <remote> <branch>
+#     partial-push = "!sh -c 'REMOTE=$0;BRANCH=$1;BATCH_SIZE=100; if git show-ref --quiet --verify refs/remotes/$REMOTE/$BRANCH; then range=$REMOTE/$BRANCH..HEAD; else range=HEAD; fi; n=$(git log --first-parent --format=format:x $range | wc -l); echo "Have to push $n packages in range of $range"; for i in $(seq $n -$BATCH_SIZE 1); do h=$(git log --first-parent --reverse --format=format:%H --skip $i -n1);  echo "Pushing $h..."; git push $REMOTE ${h}:refs/heads/$BRANCH; done; git push $REMOTE HEAD:refs/heads/$BRANCH'"
