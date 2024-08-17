35161fed77baaeed6e485017b047538853dad255
Tautvydas Rasimavicius <tautvydas.rasimavicius@gmail.com>
2024-08-17 21:37:54 +0300

    Update Telescope search patterns

--- nvim/config/lua/plugins/telescope.lua
+++ nvim/config/lua/plugins/telescope.lua
@@ -23,6 +23,30 @@ return {
             ["<M-q>"] = send_selected_to_qflist,
           },
         },
+        vimgrep_arguments = {
+          "rg",
+          "--hidden",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          -- Ignore the files matching glob patterns bellow
+          "--glob=!**/.git/*",
+          "--glob=!**/.idea/*",
+        },
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+          find_command = {
+            "rg",
+            "--files",
+            "--hidden",
+            -- Ignore the files matching glob patterns bellow
+            "--glob=!**/.git/*",
+            "--glob=!**/.idea/*",
+          },
+        },
       },
     }
   end,
