1faeac6cfaec461845d3804f8ae201f3482720d5
Tautvydas Rasimavicius <tautvydas.rasimavicius@gmail.com>
2024-08-19 16:02:03 +0300

    Disable ligatures

--- wezterm/config/wezterm.lua
+++ wezterm/config/wezterm.lua
@@ -2,7 +2,10 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "tokyonight_moon"
-config.font = wezterm.font("JetBrains Mono Nerd Font")
+config.font = wezterm.font({
+  family = "JetBrains Mono Nerd Font",
+  harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+})
 config.font_size = 11
 config.line_height = 1.1
 
