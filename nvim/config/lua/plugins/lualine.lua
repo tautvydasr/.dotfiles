9bae31cae89436f1d197266787cba50116b5a1dd
Tautvydas Rasimavicius <tautvydas.rasimavicius@gmail.com>
2025-04-19 15:54:50 +0300

    Lualine update: remove diagnostics icon, increase path length

--- nvim/config/lua/plugins/lualine.lua
+++ nvim/config/lua/plugins/lualine.lua
@@ -2,6 +2,11 @@ return {
   "nvim-lualine/lualine.nvim",
   opts = {
     sections = {
+      lualine_c = {
+        LazyVim.lualine.root_dir(),
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        { LazyVim.lualine.pretty_path({ length = 10 }) },
+      },
       lualine_z = {}, -- Hide clock
     },
   },
